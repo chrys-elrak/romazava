1586ac8ffd6c5562cb7667e50941c6634e157012
Chrys Rakotonimanana <chrysrakk@gmail.com>
2023-03-06 13:50:57 +0300

    Updating logic

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,12 @@
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 
-use crate::romazava::{Romazava};
+use crate::romazava::Romazava;
 
-pub fn command(mut romazava: Romazava) ->  Romazava {
+pub fn command(mut romazava: Romazava) -> Romazava {
     let mut m = Command::new("romazava")
+        .version("1.0")
+        .author("Chrys R. <chrysrakk@gmail.com>")
+        .about("Generate random data")
         .subcommand(
             Command::new("number")
                 .about("Generate a random number")
@@ -46,35 +49,47 @@ pub fn command(mut romazava: Romazava) ->  Romazava {
                         .short('L')
                         .long("language")
                         .required(false)
+                        .exclusive(true)
                         .help("Language of the word"),
                 ),
         );
     let matches = m.clone().get_matches();
-    if let Some(values) = matches.subcommand_matches("number") {
-        let default_range = String::from("0..10");
-        let range = values.get_one::<String>("range").unwrap_or(&default_range);
-        let range: Vec<&str> = range.split("..").map(|s| s.trim()).collect();
-        let min = range[0].parse::<i8>().unwrap_or(0);
-        let max = range[1].parse::<i8>().unwrap_or(10) + 1;
-        romazava.number(min, max);
-        return romazava;
+    match matches.subcommand() {
+        Some(("number", values)) => {
+            let default_range = String::from("0..10");
+            let range = values.get_one::<String>("range").unwrap_or(&default_range);
+            let range: Vec<&str> = range.split("..").map(|s| s.trim()).collect();
+            let min = range[0].parse::<i8>().unwrap_or(0);
+            let max = range[1].parse::<i8>().unwrap_or(10) + 1;
+            romazava.number(min, max);
+        }
+        Some(("boolean", values)) => {
+            let default_true = String::from("true");
+            let default_false = String::from("false");
+            let true_value = values
+                .get_one::<String>("true_value")
+                .unwrap_or(&default_true);
+            let false_value = values
+                .get_one::<String>("false_value")
+                .unwrap_or(&default_false);
+            romazava.boolean(true_value.to_owned(), false_value.to_owned());
+        }
+        Some(("emoji", values)) => {
+            let x = values.subcommand();
+            println!("{:?}", x);
+            romazava.emoji();
+        }
+        Some(("word", values)) => {
+            if let Some(lang) = values.get_one::<String>("language") {
+                println!("{}", lang);
+            }
+            if let Some(file) = values.get_one::<String>("file") {
+                println!("{}", file);
+            }
+        }
+        _ => {
+            m.print_help().unwrap();
+        }
     }
-    if let Some(values) = matches.subcommand_matches("boolean") {
-        let default_true = String::from("true");
-        let default_false = String::from("false");
-        let true_value = values
-            .get_one::<String>("true_value")
-            .unwrap_or(&default_true);
-        let false_value = values
-            .get_one::<String>("false_value")
-            .unwrap_or(&default_false);
-        romazava.boolean(true_value.to_owned(), false_value.to_owned());
-        return romazava;
-    }
-    if let Some(_) = matches.subcommand_matches("emoji") {
-        romazava.emoji();
-        return romazava;
-    }
-    m.print_help().unwrap();
     return romazava;
 }
